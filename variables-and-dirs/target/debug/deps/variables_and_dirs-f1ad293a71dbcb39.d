d69a31059b280d5ee087ac1f1d30a8dddd461597
IsWladi <wladimir2000@protonmail.com>
2024-09-22 23:17:27 -0300

    fix: add target dir

--- variables-and-dirs/target/debug/deps/variables_and_dirs-f1ad293a71dbcb39.d
+++
@@ -1,5 +0,0 @@
-/home/iswladi/workspace/learning_rust/variables-and-dirs/target/debug/deps/libvariables_and_dirs-f1ad293a71dbcb39.rmeta: src/main.rs
-
-/home/iswladi/workspace/learning_rust/variables-and-dirs/target/debug/deps/variables_and_dirs-f1ad293a71dbcb39.d: src/main.rs
-
-src/main.rs:
--- variables-and-dirs/target/debug/variables-and-dirs.d
+++
@@ -1,1 +0,0 @@
-/home/iswladi/workspace/learning_rust/variables-and-dirs/target/debug/variables-and-dirs: /home/iswladi/workspace/learning_rust/variables-and-dirs/src/main.rs
